bf6315bab64d92571e82602440d6bf505a8090a7
PJutch <103033969+PJutch@users.noreply.github.com>
2025-12-18 00:29:07 +0300

    Make a React frontend

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App(): React.JSX.Element {
+  return (
+    <div>
+      <h1>Hello World!</h1>
+      <p>Welcome to React with TypeScript!</p>
+    </div>
+  );
+}
+
+export default App;
