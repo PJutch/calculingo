392efc0b608a96999ecfca909fd9fcb4a249f04a
PJutch <103033969+PJutch@users.noreply.github.com>
2025-12-21 01:31:47 +0300

    Integrate MathJax

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,40 @@
 import React from 'react';
 import { supabase } from '../supabase/client';
+import { MathJax } from 'better-react-mathjax';
 import './App.css';
 
 const tasks = await supabase.from("tasks").select('*');
 console.log(tasks);
 
+interface Collection {
+  name: string;
+  previewLatex: string;
+}
+
 function App(): React.JSX.Element {
-  const premade_collections = [
-    "интегралы",
-    "ДУ",
-    "пределы",
-    "Тейлор",
-    "ряды",
-    "placeholder",
-    "placeholder",
-    "placeholder",
+  const premade_collections: Collection[] = [
+    { name: "интегралы", previewLatex: "\\int\\!{x\\,dx}" },
+    { name: "ДУ", previewLatex: "\\dot x = x" },
+    { name: "пределы", previewLatex: "\\displaystyle \\lim_{n \\to \\infty}{\\left( 1 + \\frac 1 n \\right)^n}" },
+    { name: "Тейлор", previewLatex: "\\sqrt{2}" },
+    { name: "ряды", previewLatex: "\\sqrt{2}" },
+    { name: "placeholder", previewLatex: "\\sqrt{2}" },
+    { name: "placeholder", previewLatex: "\\sqrt{2}" },
+    { name: "placeholder", previewLatex: "\\sqrt{2}" },
   ];
 
   return (
     <div className='buttons'>
-      {premade_collections.map((name, i) =>
-        <button className='button' onClick={() => alert(`TODO: implement collection ${i} page`)}>{name}</button>)}
-      <button className='browse'onClick={() => alert("TODO: implement browse custom collections page")}>Искать...</button>
-    </div>
+      {premade_collections.map((collection, i) =>
+        <button className='button' onClick={() => alert(`TODO: implement collection ${i} page`)}>
+          <p className='preview'>
+            <MathJax>\({collection.previewLatex}\)</MathJax>
+          </p>
+          <p className='name'>{collection.name}</p>
+        </button>)
+      }
+      <button className='browse' onClick={() => alert("TODO: implement browse custom collections page")}>Искать...</button>
+    </div >
   );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { MathJaxContext } from 'better-react-mathjax';
 import App from './App';
 
 const root = document.getElementById('root');
@@ -7,6 +8,8 @@ if (!root) throw new Error('Root element not found');
 
 ReactDOM.createRoot(root).render(
   <React.StrictMode>
-        <App />
+    <MathJaxContext>
+      <App />
+    </MathJaxContext>
   </React.StrictMode>
 );
