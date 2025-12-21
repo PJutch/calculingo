2a07ed3bcf8ac52ae40d0d706492a02633e0f390
PJutch <103033969+PJutch@users.noreply.github.com>
2025-12-21 13:56:54 +0300

    Add titile and improve main page

--- src/App.tsx
+++ src/App.tsx
@@ -13,28 +13,33 @@ interface Collection {
 
 function App(): React.JSX.Element {
   const premade_collections: Collection[] = [
-    { name: "интегралы", previewLatex: "\\int\\!{x\\,dx}" },
+    { name: "Интегралы", previewLatex: "\\displaystyle \\int\\!{x\\,dx}" },
     { name: "ДУ", previewLatex: "\\dot x = x" },
-    { name: "пределы", previewLatex: "\\displaystyle \\lim_{n \\to \\infty}{\\left( 1 + \\frac 1 n \\right)^n}" },
+    { name: "Пределы", previewLatex: "\\displaystyle \\lim_{n \\to \\infty}{\\left( 1 + \\frac 1 n \\right)^n}" },
     { name: "Тейлор", previewLatex: "\\sqrt{2}" },
-    { name: "ряды", previewLatex: "\\sqrt{2}" },
-    { name: "placeholder", previewLatex: "\\sqrt{2}" },
-    { name: "placeholder", previewLatex: "\\sqrt{2}" },
-    { name: "placeholder", previewLatex: "\\sqrt{2}" },
+    { name: "Ряды", previewLatex: "\\sqrt{2}" },
+    { name: "Placeholder", previewLatex: "\\sqrt{2}" },
+    { name: "Placeholder", previewLatex: "\\sqrt{2}" },
+    { name: "Placeholder", previewLatex: "\\sqrt{2}" },
   ];
 
   return (
-    <div className='buttons'>
-      {premade_collections.map((collection, i) =>
-        <button className='button' onClick={() => alert(`TODO: implement collection ${i} page`)}>
-          <p className='preview'>
-            <MathJax>\({collection.previewLatex}\)</MathJax>
-          </p>
-          <p className='name'>{collection.name}</p>
-        </button>)
-      }
-      <button className='browse' onClick={() => alert("TODO: implement browse custom collections page")}>Искать...</button>
-    </div >
+    <div className='menu-wrapper'>
+      <h1 className='title'>Calculingo</h1>
+      <div className='buttons'>
+        {premade_collections.map((collection, i) =>
+          <button className='button' onClick={() => alert(`TODO: implement collection ${i} page`)}>
+            <p className='preview'>
+              <MathJax>\({collection.previewLatex}\)</MathJax>
+            </p>
+            <p className='name'>{collection.name}</p>
+          </button>)
+        }
+      </div>
+      <div className='browse-wrapper'>
+        <button className='browse' onClick={() => alert("TODO: implement browse custom collections page")}>Искать...</button>
+      </div>
+    </div>
   );
 }
 
