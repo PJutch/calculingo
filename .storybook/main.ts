3e560b08819e4c82a6047e79425f3e9c487691ec
PJutch <103033969+PJutch@users.noreply.github.com>
2026-01-02 22:32:50 +0300

    Add Storybook

---
+++ .storybook/main.ts
@@ -0,0 +1,27 @@
+import { StorybookConfig } from '@storybook/react-vite';
+import { dirname, join } from 'path';
+
+const config: StorybookConfig = {
+  "stories": [
+    "../src/**/*.mdx",
+    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
+  ],
+  "addons": [
+    "@chromatic-com/storybook",
+    "@storybook/addon-vitest",
+    "@storybook/addon-a11y",
+    "@storybook/addon-docs",
+  ],
+  "framework": "@storybook/react-vite",
+  viteFinal: async (config) => {
+    // console.log(join(dirname(import.meta.url), 'mocks/navigate.ts'));
+    // config.resolve = config.resolve || {};
+    // config.resolve.alias = {
+    //   ...config.resolve.alias,
+    //   'react-router-dom': join(dirname(import.meta.url), 'mocks/react-router-dom.ts')
+    // };
+    return config;
+  }
+};
+
+export default config;
---
+++ src/stories/Menu.stories.tsx
@@ -0,0 +1,21 @@
+import { MathJaxContext } from 'better-react-mathjax';
+import Menu from '../Menu';
+import { MemoryRouter } from 'react-router-dom';
+
+export default {
+    title: 'Pages/Menu',
+    component: Menu,
+    decorators: [
+        (Story: React.FC) => (
+            <div style={{ width: "50%", height: "100%" }}>
+                <MemoryRouter>
+                    <MathJaxContext>
+                        <Story />
+                    </MathJaxContext>
+                </MemoryRouter>
+            </div >
+        ),
+    ],
+}
+
+export const Default = {}
