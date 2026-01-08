2a4e7d61d851e3f05e5f5988ef1993ab967deeaf
PJutch <103033969+PJutch@users.noreply.github.com>
2026-01-08 23:50:21 +0300

    Make tests less flaky

--- visual-tests/screenshot.spec.ts
+++ visual-tests/screenshot.spec.ts
@@ -1,4 +1,10 @@
-import { test, expect } from '@playwright/test';
+import { test, expect, Page } from '@playwright/test';
+
+function waitForMathJax(page: Page): Promise<void> {
+  return page.locator(':has-text("\\\\")').first().waitFor({ state: 'detached' });
+}
+
+// all this tests require running storybook
 
 test.describe('Screenshot tests', () => {
   test.beforeEach(async ({ page }) => {
@@ -6,37 +12,39 @@ test.describe('Screenshot tests', () => {
   });
 
   test('Menu test', async ({ page }) => {
-    // requires storybook started to work
     await page.goto('http://localhost:6006/iframe.html?id=pages-menu--default');
-    await page.waitForTimeout(5000); // wait some time for MathJax to load
+    await page.waitForTimeout(1000); // wait for storybook to load
+    await waitForMathJax(page);
     await expect(page).toHaveScreenshot('menu.png');
   });
 
   test('Browse test', async ({ page }) => {
     // requires storybook started to work
     await page.goto('http://localhost:6006/iframe.html?id=pages-browse--default');
-    await page.waitForTimeout(5000); // wait some time for MathJax to load
+    await page.waitForTimeout(1000); // wait for storybook to load
     await expect(page).toHaveScreenshot('browse.png');
   });
 
   test('Edit test', async ({ page }) => {
     // requires storybook started to work
     await page.goto('http://localhost:6006/iframe.html?id=pages-edit--default');
-    await page.waitForTimeout(5000); // wait some time for MathJax to load
+    await page.waitForTimeout(1000); // wait for storybook to load
+    await waitForMathJax(page);
     await expect(page).toHaveScreenshot('edit.png');
   });
 
   test('Task test', async ({ page }) => {
     // requires storybook started to work
     await page.goto('http://localhost:6006/iframe.html?id=pages-task--default');
-    await page.waitForTimeout(5000); // wait some time for MathJax to load
+    await page.waitForTimeout(1000); // wait for storybook to load
+    await waitForMathJax(page);
     await expect(page).toHaveScreenshot('task.png');
   });
 
   test('Login test', async ({ page }) => {
     // requires storybook started to work
     await page.goto('http://localhost:6006/iframe.html?id=pages-login--default');
-    await page.waitForTimeout(5000); // wait some time for MathJax to load
+    await page.waitForTimeout(1000); // wait for storybook to load
     await expect(page).toHaveScreenshot('login.png');
   });
 });
