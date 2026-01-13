cba50801d59d5e4da49870e8329a8d077f77eef4
PJutch <103033969+PJutch@users.noreply.github.com>
2026-01-14 01:05:28 +0300

    Implement LLM API access in a server function

--- supabase/functions/llm/index.ts
+++ supabase/functions/llm/index.ts
@@ -18,10 +18,31 @@ Deno.serve(async (req) => {
   if (!data) {
     throw error || new Error("Unexpected db error")
   }
+
   const formula: string = data.formula;
+  const prompt: string = `Объясни как решить ${formula}`;
+
+  const openRouterResponse = await fetch(Deno.env.get("OPENROUTER_URL") ?? "", {
+    method: "POST",
+    headers: {
+      "content-type": "application/json",
+      "Authorization": `Bearer ${Deno.env.get("OPENROUTER_API_KEY")}`
+    },
+    body: JSON.stringify({
+      messages: [{ role: "user", content: prompt }],
+      model: "deepseek/deepseek-r1-0528:free"
+    })
+  })
+
+  if (!openRouterResponse.ok) {
+    throw new Error(`OpenRouter error ${await openRouterResponse.text()}`)
+  }
+
+  const openRouterResponseBody = await openRouterResponse.json();
+  const llmReply: string = openRouterResponseBody.choices[0].message.content;
 
   return new Response(
-    JSON.stringify({ formula }),
+    JSON.stringify({ llmReply }),
     { headers: { "Content-Type": "application/json" } }
   );
 });
